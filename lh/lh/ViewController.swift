eb805864aa0ccf5b765ff7f0d28fadec5e75b9a0
liheng <1342733628@qq.com>
2015-07-21 09:52:37 +0800

    修改了，，，

--- lh/lh/ViewController.swift
+++ lh/lh/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+//        NSLog(#T##format: String##String#>, )
         // Do any additional setup after loading the view, typically from a nib.
     }
 
